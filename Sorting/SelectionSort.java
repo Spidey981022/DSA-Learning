2d7b132ba8e6b0fcb8ee70ddfee4b2f278387b0f
Tarun <tarunjaluthria.tj@gmail.com>
2026-04-09 22:46:55 +0530

    Refactor Selection Sort algorithm in Java
    Refactored selection sort implementation for clarity and correctness.

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,28 +1,58 @@
-import java.util.Arrays;
-import java.util.Scanner;
+/**
+ * Selection Sort Implementation in Java
+ * -------------------------------------
+ * This program demonstrates the Selection Sort algorithm.
+ * 
+ * Algorithm:
+ * - Iterate through the array.
+ * - For each position, find the minimum element in the unsorted part.
+ * - Swap the minimum element with the current position.
+ * 
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1) (in-place sorting)
+ * 
+ * Author: Karran
+ * GitHub Repo: DSA-Learnings
+ */
 
-public class Main {
+import java.util.Arrays;
+
+public class SelectionSortExample {
 
     public static void main(String[] args) {
-        int[] sort = {2, 4, 1, 3, 5};
-        selectionSort(sort);
-        System.out.println(Arrays.toString(sort));
+        // Example array to sort
+        int[] arr = {2, 4, 1, 3, 5};
+
+        // Call selection sort
+        selectionSort(arr);
+
+        // Print sorted array
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
 
+    /**
+     * Performs selection sort on the given array.
+     *
+     * @param arr The array to be sorted
+     * @return The sorted array
+     */
     public static int[] selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int min = arr[i];
-            for (int j = i; j < arr.length - 1; j++) {
-                if (arr[j] < min) {
-                    min = arr[j];
-                    {
-                        int temp = arr[i];
-                        arr[i] = min;
-                        arr[j] = temp;
-                    }
-                }
+        for (int i = 0; i < arr.length - 1; i++) {
+            // Assume the current index is the minimum
+            int minIndex = i;
+
+            // Find the index of the smallest element in the unsorted part
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
+
+            // Swap the found minimum element with the first unsorted element
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
         return arr;
     }
 }
